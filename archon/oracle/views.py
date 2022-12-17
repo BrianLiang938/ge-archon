f7b427c3e30a435fc01427399e7b7ca30846797f
BrianLiang938 <brian.g.liang938@gmail.com>
2022-12-17 16:06:16 -0500

    got requets to work

--- archon/oracle/views.py
+++ archon/oracle/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 import requests
 
 # Create your views here.
@@ -7,12 +8,21 @@ def index(request):
     return HttpResponse("Hello, world. You're at the oracle index.")
 
 def lookup(request, item_id):
-    api_url = "http://prices.runescape.wiki/api/v1/osrs/timeseries/id=%s/timestamp=1h" % item_id
+    api_url = "http://prices.runescape.wiki/api/v1/osrs/timeseries?id=52&timestep=1h"
     header = {
         'User-Agent': 'Price Watcher Project - @Brian#6233'
     }
     res = requests.get(api_url, headers=header)
-    return HttpResponse(res.json())
+    data = res.json()
+    fields = dict()
+    print(res.json())
+    for object in data["data"]:
+
+        fields[object['timestamp']] = object['avgHighPrice']
+    context = {
+            'fields': fields,
+    }
+    return render(request, 'oracle/index.html', context)
 
 def lookupSearch(request):
     return HttpResponse("default lookup index")
