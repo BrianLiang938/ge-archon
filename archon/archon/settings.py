a62d51b5fc7395ae9db74b8018a2b4598a200f02
BrianLiang938 <brian.g.liang938@gmail.com>
2022-12-17 16:06:20 -0500

    created requests and calls to ge api

--- archon/archon/settings.py
+++ archon/archon/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q0627^nv5df)&ff+==+v6*63da0%ge2wb^wxowy%-8nwme(+08'
+SECRET_KEY = '^bmsveiyurpnj4jj9y%btjy)!*u%xw6s*8rkq7&oy6en$ad*yq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
